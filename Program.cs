b1cd5a6ecfe3ad373c41c57bad024e398e291ece
Milagro Scardigno <mdscardigno@gmail.com>
2022-11-14 18:14:17 -0500

    public private access modifiers explanation

--- Program.cs
+++ Program.cs
@@ -2,12 +2,35 @@
 
 namespace StaticPublicPrivate
 {
-    class Program
+    class Bird
     {
+        //we can ask the constructor method to Squawk
+        //because Bird is a Bird and it can Squawk as it has access to the behavior within the Bird class but louie the object cannot
+        public Bird()
+        {
+            Squawk();
+        }
+        //behavior 
+        //considered private and only accessible within the class. Only other instances of this class can access behaviors in this class Bird
+        //In other words, other Bird methods can access this method but not other classes
+        //Solution: make Squawk() public
+        public void Squawk()
+        {
+            Console.WriteLine("Squawk");
+        }
+        //why would we use private methods?
+        //to hide the implementation details of a method from the outside world
+    }
+    class Program
+    {//static means that the method Main can be call without an instance of the class Program
         static void Main(string[] args)
         {
+            var louie = new Bird();
+            louie.Squawk();//louie, the object performs its behavior or method Squawk
             Console.WriteLine("Static Public Private");
 
         }
     }
+
+    //there is also protected access modifier
 }
